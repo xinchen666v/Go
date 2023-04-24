83223177bd8f958517a9292647dad4d7ff4e90da
xinchen666v <77438958+xinchen666v@users.noreply.github.com>
2023-04-25 01:22:36 +0800

    添加了自定义函数，和函数的调用

--- hello.go
+++ hello.go
@@ -2,6 +2,11 @@ package main
 
 import "fmt"
 
+// 程序的入口点
 func main() {
 	fmt.Println("Hello, world!！")
+	say("Hello Go!") //调用函数
+}
+func say(message string) {
+	fmt.Println("You said:", message)
 }
