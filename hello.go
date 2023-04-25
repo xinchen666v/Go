136cefaa6cf6ee793820bf97c8032235905be646
xinchen666v <77438958+xinchen666v@users.noreply.github.com>
2023-04-25 13:51:54 +0800

    添加了字符串的定义，字符串长度的打印

--- hello.go
+++ hello.go
@@ -1,11 +1,14 @@
 package main
 
-import "fmt"
+import "fmt" //导入IO库
 
 // 程序的入口点
 func main() {
+	s1 := "Hello" + "World" //定义字符串
 	fmt.Println("Hello, world!！")
-	say("Hello Go!") //调用函数
+	say("Hello Go!")             //调用函数
+	fmt.Println(len(s1))         //打印字符串的长度
+	fmt.Println(string(s1[0:5])) //打印字符串的前五个字符
 }
 func say(message string) {
 	fmt.Println("You said:", message)
